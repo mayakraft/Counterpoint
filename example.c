ec34fc8b3b8183e8a1c5e2438db9f92331be013b
robbykraft <robbykraft@gmail.com>
2014-08-07 15:00:23 -0700

    successful harmony, most-oblique, least-motion

--- example.c
+++ example.c
@@ -3,18 +3,26 @@
 #define NOTES 11
 
 void makeCounterpoint(){
+    
+    // -999 is code for NULL
 
-
+    // root = 1
     int cantusFirmus[NOTES] = {1, 3, 2, 1, 4, 3, 5, 4, 3, 2, 1};
 
-    printf("\ncantus firmus:\n  ");
+    printf("\ncantus firmus:\n");
     for(int i = 0; i < 11; i++)
         printf(" %d  ",cantusFirmus[i]);
     printf("\n\n");
 
+    // root = 0
+    for(int i = 0; i < NOTES; i++)
+        cantusFirmus[i]--;
     
+    
+    
+    // 0:DO  1:RE  2:MI  3:FA  4:SOL  5:LA  6:TI
+
     int consonantHarmonies[NOTES*8]; // four up, four down, for each note
-    
     for(int i = 0; i < NOTES; i++){
         // four above
         consonantHarmonies[i*8+0] = cantusFirmus[i] + 3 - 1;
@@ -26,16 +34,29 @@ void makeCounterpoint(){
         consonantHarmonies[i*8+5] = cantusFirmus[i] - 5 + 1;
         consonantHarmonies[i*8+6] = cantusFirmus[i] - 6 + 1;
         consonantHarmonies[i*8+7] = cantusFirmus[i] - 8 + 1;
-        
-        // because -1 is the same as 1, -2 is one step away, so is +2
-        for(int j = 4; j < 8; j++)
-            if(consonantHarmonies[i*8+j] < 1)
-                consonantHarmonies[i*8+j] -= 2;
-        
-//        consider fixing this, the root of cantus firmus should be 0
-        
     }
     
+    
+    
+    printf("consonant harmonies:\n");
+    for(int j = 0; j < 8; j++){
+        for(int i = 0; i < NOTES; i++){
+            if(consonantHarmonies[i*8+j] == -999)
+                printf("     ");
+            else if(consonantHarmonies[i*8+j] < 9 && consonantHarmonies[i*8+j] >= 0)
+                printf(" %d  ", consonantHarmonies[i*8+j] + 1);
+            else if(consonantHarmonies[i*8+j] < -9)
+                printf("%d ", consonantHarmonies[i*8+j] - 1);
+            else if(consonantHarmonies[i*8+j] < 0)
+                printf("%d  ", consonantHarmonies[i*8+j] - 1);
+            else
+                printf(" %d ", consonantHarmonies[i*8+j] + 1);
+            
+        }
+        printf("\n");
+    }
+    printf("\n");
+
 //    now build the idea of the interval step
     
     interval intervals[NOTES-1];
@@ -51,49 +72,79 @@ void makeCounterpoint(){
             intervals[i].d = -1;
     }
     
+    
+    
+    int counterPoint[NOTES];
+    counterPoint[0] = 5 - 1; // start on fifth
+    
     for(int i = 1; i < NOTES; i++){
         short motion = intervals[i-1].d;
         
+        // find a next note
+        int possibleNextNotes[8];
+        int bestNoteIndex = -1;
+        int finalAnswer = -1;
+        
         for(int j = 0; j < 8; j++){
-            short attemptedMotion = consonantHarmonies[i*i+0];
-            if(motion == -1 && attemptedMotion == -1){
-//                this thing trying to to direct motion
-                if(consonantHarmonies[i*8+j] - cantusFirmus[i] == 0 ||
-                   consonantHarmonies[i*8+j] - cantusFirmus[i] == 5 ||
-                   consonantHarmonies[i*8+j] - cantusFirmus[i] == 8 ||
-                   consonantHarmonies[i*8+j] - cantusFirmus[i] == -5 ||
-                   consonantHarmonies[i*8+j] - cantusFirmus[i] == -8  )
-                {
-                    // this means we are doing direct motion into a perfect consonant
-                    //forbidden
-                    consonantHarmonies[i*8+j] = -999;
+            if(finalAnswer == -1){
+                possibleNextNotes[j] = consonantHarmonies[i*8+j];
+                short attemptedMotion;
+                // will be oblique motion
+                if(counterPoint[i-1] == possibleNextNotes[j])
+                    attemptedMotion = 0;
+                // will be downward
+                else if(counterPoint[i-1] > possibleNextNotes[j])
+                    attemptedMotion = -1;
+                // will be upward
+                else if(counterPoint[i-1] < possibleNextNotes[j])
+                    attemptedMotion = 1;
+                
+                if(attemptedMotion == 0)
+                    bestNoteIndex = finalAnswer = j;
+                
+                if((motion == -1 && attemptedMotion == -1) || (motion == 1 && attemptedMotion == 1)){
+                    // this thing trying to to direct motion
+                    // check if we are trying for a perfect interval
+                    int interval1 = abs(cantusFirmus[i] - possibleNextNotes[j]);
+                    int interval2 = abs(possibleNextNotes[j] - cantusFirmus[i]);
+                    
+                    if(cantusFirmus[i] - possibleNextNotes[j] == 0 ||
+                       possibleNextNotes[j] == 4 ||
+                       possibleNextNotes[j] == 7 ||
+                       possibleNextNotes[j] == -4 ||
+                       possibleNextNotes[j] == -7  )
+                    {
+                        // direct motion into a perfect consonance
+                        // this is not allowed
+                        possibleNextNotes[j] = -999;
+                    }
                 }
-                else{
-                    // everything else is okay
+                if(bestNoteIndex == -1) bestNoteIndex = j;
+                else if(possibleNextNotes[j] != -999){
+                    if( abs(cantusFirmus[i] - possibleNextNotes[j]) < abs(cantusFirmus[i] - possibleNextNotes[bestNoteIndex]) )
+                        bestNoteIndex = j;
                 }
             }
+            counterPoint[i] = possibleNextNotes[bestNoteIndex];
         }
     }
     
-    printf("consonant harmonies:\n");
-    for(int j = 0; j < 8; j++){
-        printf("| ");
-        for(int i = 0; i < NOTES; i++){
-            if(consonantHarmonies[i*8+j] == -999)
-                printf("     ");
-            else if(consonantHarmonies[i*8+j] < 10 && consonantHarmonies[i*8+j] >= 0)
-                printf(" %d  ", consonantHarmonies[i*8+j]);
-            else if(consonantHarmonies[i*8+j] < -9)
-                printf("%d ", consonantHarmonies[i*8+j]);
-            else if(consonantHarmonies[i*8+j] < 0)
-                printf("%d  ", consonantHarmonies[i*8+j]);
-            else
-                printf(" %d ", consonantHarmonies[i*8+j]);
-            
-        }
-        printf("\n");
+    
+    printf("counterpoint:\n");
+    for(int i = 0; i < NOTES; i++){
+        if(counterPoint[i] == -999)
+            printf("     ");
+        else if(counterPoint[i] < 10 && counterPoint[i] >= 0)
+            printf(" %d  ", counterPoint[i] + 1);
+        else if(counterPoint[i] < -9)
+            printf("%d ", counterPoint[i] - 1);
+        else if(counterPoint[i] < 0)
+            printf("%d  ", counterPoint[i] - 1);
+        else
+            printf(" %d ", counterPoint[i] + 1);
     }
-    printf("\n");
+    printf("\n\n\n");
+
     
     ///////////////////////////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////////////////////
@@ -133,7 +184,7 @@ void makeCounterpoint(){
         }
         
     }
-    int counterPoint[11];
+//    int counterPoint[11];
     counterPoint[0] = 5;
     for(int i = 1; i < 11; i++){
         if(steps[i-1].toNote.consonance && steps[i-1].toNote.perfect){
